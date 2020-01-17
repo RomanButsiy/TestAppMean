c5b3803e7e11dfc496062539ea27cb091b5a63e4
RomanButsiy <butsiyroman@gmail.com>
2020-01-17 12:49:05 +0200

    Added user model

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.get('/reg', (req, res) => {
-  res.send('Це сторінка реєстрації');
+// router.get('/reg', (req, res) => {
+//   res.send('Це сторінка реєстрації');
+// });
+
+router.post('/reg', (req, res) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    login: req.body.login,
+    password: req.body.password
+  });
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({success: false, msg: 'Користувача небуло добавлено'});
+    } else {
+      res.json({success: true, msg: 'Користувача було добавлено'})
+    }
+  })
 });
 
 router.get('/auth', (req, res) => {
